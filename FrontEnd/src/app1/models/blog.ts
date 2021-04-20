0ccf789293c8b415c969221c4368d16e55c1060e
RodrigoDiaz10 <raa.diaz@yavirac.edu.ec>
2021-04-19 20:31:34 -0500

    borre la carpeta app1

--- FrontEnd/src/app1/models/blog.ts
+++
@@ -1,9 +0,0 @@
-export class Blog {
-    constructor(
-        public title: string, 
-        public description: string, 
-        public image?: string,
-        public link?: string, 
-        public id?: number
-        ){}
-  }
